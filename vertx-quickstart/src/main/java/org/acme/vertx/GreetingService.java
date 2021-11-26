1018abb8c2480db1ba66ebd6fe378618dfc35ec5
bojernico <nico.bojer@gmail.com>
2021-11-26 12:41:59 +0100

    added event resource

---
+++ vertx-quickstart/src/main/java/org/acme/vertx/GreetingService.java
@@ -0,0 +1,26 @@
+package org.acme.vertx;
+
+import io.quarkus.vertx.ConsumeEvent;
+import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.context.ManagedExecutor;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class GreetingService {
+
+    @Inject
+    ManagedExecutor executor;
+
+    @ConsumeEvent("greeting")
+    public String consume(String name) {
+        return name.toUpperCase();
+    }
+
+    @ConsumeEvent("greeting")
+    public Uni<String> consume2(String name) {
+        return Uni.createFrom().item(name::toUpperCase).emitOn(executor);
+    }
+}
